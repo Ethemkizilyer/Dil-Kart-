aeb26ed163a7138c9d2d5b5da7e7477e076cef53
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2022-09-26 13:13:57 +0300

    initialcommit

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+const addBtn = document.querySelector(".eklem");
+const silBtn = document.querySelector(".sil");
+const ing = document.querySelector(".ing");
+const tr = document.querySelector(".tr");
+const eksil = document.querySelector(".eksil");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const cerceve = document.querySelector(".cerceve");
 const sayı = document.querySelector(".span");
+
 let a = 0;
 let asd = 0;
 let elf = 1;
-// console.log(asd);
+
+let local = JSON.parse(localStorage.getItem("LOCAL")) || [];
+
 left.addEventListener("click", () => {
   if (asd == 0) {
     left.disabled = "";
@@ -15,260 +23,126 @@ left.addEventListener("click", () => {
     asd += 200;
     cerceve.style.transform = `translateX(${asd}px)`;
   }
+
   sayı.innerText = elf;
+  localStorage.setItem("LOCAL", JSON.stringify(local));
 });
 right.addEventListener("click", () => {
-  // if (cerceve.children.length == 0) {
-  //   alert("Kart Boş");
-  //   elf = 1;
-  //   sayı.innerText = elf;
-  //   right.disabled = "";
-  // }
-  if (asd == (a - 1) * -200) {
+  if (elf >= local.length) {
     elf = 1;
     asd = 0;
     cerceve.style.transform = `translateX(${asd}px)`;
-    //  right.disabled = "";
-    console.log("burada");
+
     sayı.innerText = elf;
-    console.log(a);
   } else {
-    if(cerceve.children.length == 0){
-      asd = 0
-      alert("Kart Boş");
-      elf = 1
-      sayı.innerText = elf;
-      a= 0;
-      console.log(asd);
-      console.log(a);
-      console.log(elf);
-      
-    }
-    else{
+    sayı.innerText = elf;
+
     asd -= 200;
-    // asd = a * -200;
+
     elf++;
     cerceve.style.transform = `translateX(${asd}px)`;
-    console.log(asd);
-    console.log(a);
-    sayı.innerText = elf;}
-    
-  }
-});
-right.addEventListener("dblclick", () => {
-  // if (cerceve.children.length == 0) {
-  //   alert("Kart Boş");
-  //   elf = 1;
-  //   sayı.innerText = elf;
-  //   right.disabled = "";
-  // }
-  if (asd == (a - 1) * -200) {
-    elf = 1;
-    asd = 0;
-    cerceve.style.transform = `translateX(${asd}px)`;
-    //  right.disabled = "";
-    console.log("burada");
+
     sayı.innerText = elf;
-    console.log(a);
-  } else {
-    if(cerceve.children.length == 0){
-      asd = 0
-      alert("Kart Boş")
-      elf = 1
-      sayı.innerText = elf;
-      a= 0
-    }
-    else{
-    asd -= 200;
-    // asd = a * -200;
-    elf++;
-    cerceve.style.transform = `translateX(${asd}px)`;
-    console.log(asd);
-    console.log(a);
-    sayı.innerText = elf;}
-    
   }
+  localStorage.setItem("LOCAL", JSON.stringify(local));
 });
 
-const ing = document.querySelector(".ing");
-const tr = document.querySelector(".tr");
-const eklem = document.querySelector(".eklem");
-const sil = document.querySelector(".sil");
-
-eklem.addEventListener("click", () => {
-  if (!tr.value || !ing.value) {
-    alert("Eksik Giriş");
-  } else {
-    a += 1;
-    console.log(a);
-    console.log(asd);
-    const ana = document.createElement("div");
-    ana.classList.add("ana");
-    const kapsa = document.createElement("div");
-    kapsa.classList.add("kapsa");
-    const ilk = document.createElement("div");
-    ilk.classList.add("ilk");
-    const arka = document.createElement("div");
-    arka.classList.add("arka");
-    const p1 = document.createElement("p");
-    const p2 = document.createElement("p");
-    ilk.appendChild(p1);
-    arka.appendChild(p2);
-    kapsa.appendChild(arka);
-    kapsa.appendChild(ilk);
-    ana.appendChild(kapsa);
-    cerceve.appendChild(ana);
-    // console.log(ana);
-    // console.log("merhaba");
-
-    function ethem(item) {
-      const mySentence = item;
-      const words = mySentence.split(" ");
-
-      let capit = words
-        .map((word) => {
-          return word[0].toUpperCase() + word.substring(1);
-        })
-        .join(" ");
-      return capit;
-    }
-
-    p1.innerText = ethem(ing.value);
-    // console.log(ing.value);
-    p2.innerText = ethem(tr.value);
-    // console.log(tr.value);
-  }
-  tr.value = "";
-  ing.value = "";
-});
-
-sil.addEventListener("click", (e) => {
-  // elf--;
-  // a--;
-  console.log("gör");
-
-  sayı.innerText = elf;
-  if (cerceve.children.length == 0) {
-    right.disabled = "";
-    alert("Kart Boş");
-    elf = 1;
-    a = 0;
-    asd = 0
-    sayı.innerText = elf;
-    
-  } else {
-    cerceve.children[0].remove();
-  }
-
-  // console.log(cerceve.children.length);
-});
-
-function myFunction(event) {
-  event.target.addEventListener("click", () => {
-    // console.log(event.target.className);
-    // if(event.target.className == "v"){
-    // event.target.classList.toggle("cizik");
-    // console.log(event.target.firstChild);}
-    // console.log(event.target.children[0].children);
-    var x = event.target;
-    event.target.addEventListener("dblclick", () => {
-      var x = event.target;
-      // console.log(x);
-      // a--;
-      // a--;
-      if (x.parentElement.className == "kapsa") {
-        event.target.parentElement.parentElement.remove();
-        
-        console.log("gör");
-
-        sayı.innerText = elf;
-      }
-    });
-    if (x.tagName == "P") {
-      event.target.classList.toggle("cizik");
-      event.target.parentElement.nextElementSibling.children[0].classList.toggle(
-        "cizik"
-      );
-      //   event.target.previousElementSibling.children.classList.toggle("cizik");
-      //   event.target.nextElementSibling.children[0].classList.toggle("cizik");
-    }
+const renderSavedTodos = () => {
+  local.forEach((ana) => {
+    createListElement(ana);
   });
+};
+renderSavedTodos();
+
+function createListElement(newAna) {
+  const { id, completed, text1, text2 } = newAna;
+
+  const ana = document.createElement("div");
+  ana.setAttribute("id", id);
+  ana.classList.add("ana");
+
+  completed && ana.classList.add("checked");
+
+  const kapsa = document.createElement("div");
+  kapsa.classList.add("kapsa");
+  ana.appendChild(kapsa);
+  const ilk = document.createElement("div");
+  ilk.classList.add("ilk");
+  kapsa.appendChild(ilk);
+  const arka = document.createElement("div");
+  arka.classList.add("arka");
+  kapsa.appendChild(arka);
+  const p1 = document.createElement("p");
+  const pText1 = document.createTextNode(text1);
+  p1.appendChild(pText1);
+  ilk.appendChild(p1);
+  const p2 = document.createElement("p");
+  const pText2 = document.createTextNode(text2);
+  p2.appendChild(pText2);
+  arka.appendChild(p2);
+  cerceve.appendChild(ana);
 }
 
-// ******************************LOCAL??**********************
+cerceve.addEventListener("click", (e) => {
+  e.target.addEventListener("click", () => {
+    e.target.parentElement.parentElement.parentElement.classList.toggle(
+      "checked"
+    );
+    local.map((elem, index) => {
+      if (
+        elem.id ==
+        e.target.parentElement.parentElement.parentElement.getAttribute("id")
+      ) {
+        local[index].completed = !local[index].completed;
+      }
+    });
+    localStorage.setItem("LOCAL", JSON.stringify(local));
+  });
+});
 
-// function saveLocalTodos(todo) {
-//   let todos;
-//   if (localStorage.getItem("todos") === null) {
-//     todos = [];
-//   } else {
-//     todos = JSON.parse(localStorage.getItem("todos"));
-//   }
+eksil.addEventListener("click", (e) => {
+  a--;
+  if (e.target.classList.contains("sil")) {
+    sayı.innerText = elf;
+    if (local.length == 0) {
+      //   right.disabled = "";
+      alert("Kart Boş");
+      elf = 1;
+      a = 0;
+      asd = 0;
+      sayı.innerText = elf;
+    } else {
+      const id =
+        e.target.parentElement.parentElement.previousElementSibling.children[0].children[0].children[0].getAttribute(
+          "id"
+        );
 
-//   todos.push(todo);
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// }
+      e.target.parentElement.parentElement.previousElementSibling.children[0].children[0].children[0].remove();
 
-// function getTodos() {
-//   let todos;
-//   if (localStorage.getItem("todos") === null) {
-//     todos = [];
-//   } else {
-//     todos = JSON.parse(localStorage.getItem("todos"));
-//   }
+      local = local.filter((ana) => ana.id !== Number(id));
+    }
 
-//   todos.forEach((todo) => {
-//     // CREATE TODO DİV
+    localStorage.setItem("LOCAL", JSON.stringify(local));
+  } else if (e.target.classList.contains("eklem")) {
+    a++;
+    if (ing.value.trim() === "" || tr.value.trim() === "") {
+      alert("Eksik Giriş.Lütfen Tekrar Deneyiniz!!!");
+    } else {
+      const newAna = {
+        id: new Date().getTime(),
+        completed: false,
+        text1: ing.value,
+        text2: tr.value,
+      };
 
-//     a += 1;
-//     const ana = document.createElement("div");
-//     ana.classList.add("ana");
-//     const kapsa = document.createElement("div");
-//     kapsa.classList.add("kapsa");
-//     const ilk = document.createElement("div");
-//     ilk.classList.add("ilk");
-//     const arka = document.createElement("div");
-//     arka.classList.add("arka");
-//     const p1 = document.createElement("p");
-//     const p2 = document.createElement("p");
-//     ilk.appendChild(p1);
-//     arka.appendChild(p2);
-//     kapsa.appendChild(arka);
-//     kapsa.appendChild(ilk);
-//     ana.appendChild(kapsa);
-//     cerceve.appendChild(ana);
-//     console.log(ana);
-//     console.log("merhaba");
+      createListElement(newAna);
 
-//  function ethem(item) {
-//       const mySentence = item;
-//       const words = mySentence.split(" ");
+      local.push(newAna);
 
-//       let capit = words
-//         .map((word) => {
-//           return word[0].toUpperCase() + word.substring(1);
-//         })
-//         .join(" ");
-//       return capit;
-//     }
+      localStorage.setItem("LOCAL", JSON.stringify(local));
 
-//     p1.innerText = ethem(ing.value);
-//     // console.log(ing.value);
-//     p2.innerText = ethem(tr.value);
-//     // console.log(tr.value);
-//   })
-//   tr.value = "";
-//   ing.value = "";
-// }
-// function removeLocaleStorage(todo) {
-//   let todos;
-//   if (localStorage.getItem("todos") === null) {
-//     todos = {};
-//   } else {
-//     todos = JSON.parse(localStorage.getItem("todos"));
-//   }
-
-//   const todoIndex = todo.children[1].innerText;
-//   todos.splice(todos.indexOf(todoIndex), 1);
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// }
+      ing.value = "";
+      tr.value = "";
+    }
+  }
+});
